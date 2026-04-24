a792884bfe34d852ee0f63cb245a01305cc9522d
AleksandrLakman <sashkalakman@gmai.com>
2026-04-24 11:35:56 +0600

    pr3

---
+++ 922-master/922-master/Практическая работа № 3/Задание_1/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+
+        echo '<pre>';
+
+        $words = ['Иванов', 'Иван', 'Иванович', '15.05.1985', 'Учитель математики', 'Высшая', 'Высшее(магистратура)', 'МГУ им. М.В.Ломоносова', 'Математик, преподаватель', 'Математика и информатика', '5 лет', '12 лет', 'ivanov_i@example.com'];
+        print_r($words);
+
+        echo '</pre>';
+
+
+    ?>
+    
+</body>
+</html>
